09c727bf47360bc2db266111388dfe9745d7fed0
Administrator <murillowebdesigner@gmail.com>
2024-12-09 19:56:03 -0300

    Add project files.

---
+++ Consumer2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    await Task.Delay(10000);
+
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+};
+
+Console.WriteLine("Consumer 2");
+await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+
+        using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+
+        int count = 0;
+        while (true)
+        {
+            string message = $"MyMessage. Count: {count}";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+            Console.WriteLine($" [x] Sent {message}");
+
+            count++;
+
+            await Task.Delay(5000); 
+        }
+    }
+}
